85f548bd4b3108a468e960d65648f21a1449a9d0
Vasylij Kashtalyan <88617002+Vasylij-Kashtalyan@users.noreply.github.com>
2024-03-09 12:01:58 +0200

    price plans

--- js/changeLetGo.js
+++ js/changeLetGo.js
@@ -1,34 +1,26 @@
-const ref = document.querySelector(".letgo-brows");
-const letGo = document.querySelector(".letgo");
+// const ref = document.querySelector(".letgo-brows");
+// const letGo = document.querySelector(".letgo");
 
-console.log(letGo);
+// console.log(letGo);
 
-let words = ["EYEBROWS", "EYELASHES"];
-
-setInterval(() => {
-  // opOne();
-
-  ref.textContent = "ВІЇ";
-  // letGo.style.backgroundImage = 'url("../img/beautiful-woman.jpg")';
-}, 4000);
-
-setInterval(() => {
-  ref.textContent = "БРОВІ";
-  // letGo.style.backgroundImage = 'url("../img/eyebrows.jpg")';
-}, 8000);
+// let words = ["EYEBROWS", "EYELASHES"];
 
 // setInterval(() => {
-//   opOne();
+//   // opOne();
+
+//   ref.textContent = "ВІЇ";
+//   // letGo.style.backgroundImage = 'url("../img/beautiful-woman.jpg")';
 // }, 4000);
 
 // setInterval(() => {
-//   opZer();
-// }, 5500);
+//   ref.textContent = "БРОВІ";
+//   // letGo.style.backgroundImage = 'url("../img/eyebrows.jpg")';
+// }, 6000);
 
-// function opZer() {
-//   ref.style.opacity = "0";
-// }
+// // setInterval(() => {
+// //   ref.style.opacity = "1";
+// // }, 6000);
 
-// function opOne() {
-//   ref.style.opacity = "1";
-// }
+// // setInterval(() => {
+// //   ref.style.opacity = "0";
+// // }, 4000);
